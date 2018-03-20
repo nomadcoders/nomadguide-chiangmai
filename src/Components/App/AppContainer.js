e1220f32bf7fae433cfc7558b700c93eae6a47bc
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-20 15:20:32 +0900

    Parcel Configuration. Gonna build everything by hand@

---
+++ src/Components/App/AppContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AppPresenter from "./AppPresenter";
+
+class AppContainer extends React.Component {
+  render() {
+    return <AppPresenter />;
+  }
+}
+
+export default AppContainer;
