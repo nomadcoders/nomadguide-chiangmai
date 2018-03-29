25eaa74a73cf97bea1202379dc30be37d2bc2c0c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-29 11:03:34 +0200

    Programmer

--- src/content/DevCommunity.js
+++ src/content/DevCommunity.js
@@ -4,38 +4,41 @@ import { Heading, Box, Text } from "gestalt";
 
 export default () => (
   <Fragment>
-    <MaskImage photo={require("../assets/community/01.jpg")} />
+    <MaskImage photo={require("../assets/community/02.jpg")} />
     <Box marginTop={5} marginBottom={5}>
-      <Heading size="xs">주택</Heading>
+      <Heading size="xs">개발자 커뮤니티</Heading>
     </Box>
     <Box marginTop={5} marginBottom={5}>
       <Text>
-        태국 현지인들이 가족 단위로 거주하는 주택을 무반 (Moo Baan)이라고 한다.
-        방 2~4개, 거실, 부엌, 조리시설, 정원, 주차장이 있다. 주택의 경우 최소
-        계약 기간이 6개월이며 위치도 시내에서 떨어진 항동에 위치하여 있다. 가족
-        단위로 치앙마이에 거주하는 외국인들의 경우 국제학교 인근에 있는 주택
-        단지 (village, gated community)에 모여 산다. 주택 단지의 경우 사설
-        경비원 그리고 수영장 및 체육시설을 갖추고 있다. 한 달 렌트비는 계약
-        기간에 따라 상이하나 보통 20,000밧부터 시작한다.
+        프로그래밍, 비트코인 등 개발자 커뮤니티가 활발하다. 거의 매주 한 번씩은
+        관련 모임이 있다. 특히 비트코인 모임의 경우에는 2014년부터 꾸준히 매주
+        운영되었다. 필자는 개발자가 아닌지라 해당 커뮤니티 참석을 해본 적이
+        없다. 그러나 옆에서 지켜본 바에 의하면 비트코인 밋업은 꾸준히
+        고정멤버들에 의하여 잘 운영되고 있는 듯 하다.
       </Text>
     </Box>
     <Box marginTop={5} marginBottom={5}>
       <Text>
-        <li>
-          <b>가격대:</b> 최소 20,000 밧 이상.
-        </li>
-        <li>
-          <b>시설:</b> 방 (2~4개), 거실, 부엌, 정원 등.
-        </li>
-        <li>
-          <b>지역:</b> 항동.
-        </li>
-        <li>
-          <b>계약기준:</b> 최소 6개월 이상.
-        </li>
-        <li>
-          <b>보증금:</b> 1개월 월세.
-        </li>
+        <b>비트코인 밋업</b>
+        <SLink href="https://www.meetup.com/bitcoinsinchiangmai/">
+          Website
+        </SLink>
+      </Text>
+    </Box>
+    <Box marginTop={5} marginBottom={5}>
+      <Text>
+        <b>워드프레스 밋업</b>
+        <SLink href="https://www.meetup.com/Chiang-Mai-WordPress-Meetup">
+          Website
+        </SLink>
+      </Text>
+    </Box>
+    <Box marginTop={5} marginBottom={5}>
+      <Text>
+        <b>웹 프로그래머 밋업:</b>
+        <SLink href="https://www.meetup.com/Chiang-Mai-Web-Programmer/">
+          Website
+        </SLink>
       </Text>
     </Box>
   </Fragment>
