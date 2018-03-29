b5399cb9aabad1a0b0a9c546e643aaa851b66fcb
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-29 11:16:20 +0200

    Yoga community

--- src/content/YogaCommunity.js
+++ src/content/YogaCommunity.js
@@ -4,38 +4,82 @@ import { Heading, Box, Text } from "gestalt";
 
 export default () => (
   <Fragment>
-    <MaskImage photo={require("../assets/housing/06.jpg")} />
+    <MaskImage photo={require("../assets/community/03.jpg")} />
     <Box marginTop={5} marginBottom={5}>
-      <Heading size="xs">주택</Heading>
+      <Heading size="xs">요가 커뮤니티</Heading>
     </Box>
     <Box marginTop={5} marginBottom={5}>
       <Text>
-        태국 현지인들이 가족 단위로 거주하는 주택을 무반 (Moo Baan)이라고 한다.
-        방 2~4개, 거실, 부엌, 조리시설, 정원, 주차장이 있다. 주택의 경우 최소
-        계약 기간이 6개월이며 위치도 시내에서 떨어진 항동에 위치하여 있다. 가족
-        단위로 치앙마이에 거주하는 외국인들의 경우 국제학교 인근에 있는 주택
-        단지 (village, gated community)에 모여 산다. 주택 단지의 경우 사설
-        경비원 그리고 수영장 및 체육시설을 갖추고 있다. 한 달 렌트비는 계약
-        기간에 따라 상이하나 보통 20,000밧부터 시작한다.
+        노마드, 프로그래밍 보다 더 활성화된 커뮤니티는 요가, 명상, 타이 마사지
+        커뮤니티이다. 사실 치앙마이에 가장 처음 생긴 커뮤니티는 요가, 명상
+        커뮤니티이기에 어찌 보면 더 역사가 오래되었고 커뮤니티도 더욱 돈독하다.
+        필자는 치앙마이 도착하여 무에타이, 퍼머컬쳐, 요가를 배우며 비슷한
+        관심사를 지닌 친구들을 만날 수 있었다.
+      </Text>
+    </Box>
+    <MaskImage photo={require("../assets/articles/temple.jpg")} />
+    <Box marginTop={5} marginBottom={5}>
+      <Heading size="xs">요가:</Heading>
+    </Box>
+    <Box marginTop={5} marginBottom={5}>
+      <Text>
+        <b>Yoga Tree:</b>
+        <Text inline={false}>
+          <br />
+          요가 뿐만 아니라 댄스 만델라 (Dance Mandala)등 다양한 워크숍을
+          운영하고 있다. 1회 수업마다 300밧, 10회 수강권은 2,500밧이다.
+        </Text>
+        <br />
+        <SLink href="http://theyogatree.org/">Website</SLink>
       </Text>
     </Box>
     <Box marginTop={5} marginBottom={5}>
       <Text>
-        <li>
-          <b>가격대:</b> 최소 20,000 밧 이상.
-        </li>
-        <li>
-          <b>시설:</b> 방 (2~4개), 거실, 부엌, 정원 등.
-        </li>
-        <li>
-          <b>지역:</b> 항동.
-        </li>
-        <li>
-          <b>계약기준:</b> 최소 6개월 이상.
-        </li>
-        <li>
-          <b>보증금:</b> 1개월 월세.
-        </li>
+        <b>Tantra Yoga:</b>
+        <Text inline={false}>
+          <br />
+          요가, 요가 리트릿 중심으로 운영하고 있다. 1회 수업 220밧, 한달
+          수강권은 700밧이다. 초보자는 morning/evening yoga 를 수강하면 된다.
+        </Text>
+        <br />
+        <SLink href="http://www.tantrayogathailand.com/">Website</SLink>
+      </Text>
+    </Box>
+    <Box marginTop={5} marginBottom={5}>
+      <Heading size="xs">명상:</Heading>
+    </Box>
+    <Box marginTop={5} marginBottom={5}>
+      <Text>
+        <b>Wat Umong:</b>
+        <Text inline={false}>
+          <br />
+          왓 우몽 사원에서 비파사나 명상을 배우며 머물 수 있다. 최소 일주일
+          머물며 수련해야하며 하루 220밧을 내면 사원에서 잠자리 및 식사를
+          제공해준다. 외국인 수련자들을 고려하여 영어로 명상 프로그램을
+          진행한다.
+        </Text>
+        <br />
+        <SLink href="http://www.watumong.org/web/187/wat-umong-meditation-center">
+          Website
+        </SLink>
+      </Text>
+    </Box>
+    <Box marginTop={5} marginBottom={5}>
+      <Heading size="xs">타이 마사지:</Heading>
+    </Box>
+    <Box marginTop={5} marginBottom={5}>
+      <Text>
+        <b>Thai Massage Shivagakomarpaj:</b>
+        <Text inline={false}>
+          <br />
+          치앙마이에서 타이 마사지를 배울 수 있는 곳은 매우 많다. 너무 저렴한
+          곳을 찾기보다는, 전문적으로 꾸준히 학생들을 배출하고 있고, 집에서
+          가까운 곳에서 최소 1주일 정통 태국 마사지 코스를 통해 배우기를
+          추천한다. 오일 마사지를 배우는 것보다, 전통 태국 마사지를 배우는 것이
+          좋으며, 이왕 배운다면 최소 1주일은 배워야 기본기를 닦을 수 있다.
+        </Text>
+        <br />
+        <SLink href="http://thaimassageschool.ac.th/">Website</SLink>
       </Text>
     </Box>
   </Fragment>
